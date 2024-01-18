bd2d1b4d045490edf5e49e5cd46fac52bfd8d4c5
Victor Kareh <vkareh@redhat.com>
2024-01-18 15:13:19 -0500

    sleep-tracker

--- src/components/ble/MotionService.h
+++ src/components/ble/MotionService.h
@@ -18,6 +18,7 @@ namespace Pinetime {
       int OnStepCountRequested(uint16_t attributeHandle, ble_gatt_access_ctxt* context);
       void OnNewStepCountValue(uint32_t stepCount);
       void OnNewMotionValues(int16_t x, int16_t y, int16_t z);
+      void OnNewSleepValues(bool sleepValue);
 
       void SubscribeNotification(uint16_t attributeHandle);
       void UnsubscribeNotification(uint16_t attributeHandle);
@@ -27,13 +28,15 @@ namespace Pinetime {
       NimbleController& nimble;
       Controllers::MotionController& motionController;
 
-      struct ble_gatt_chr_def characteristicDefinition[3];
+      struct ble_gatt_chr_def characteristicDefinition[4];
       struct ble_gatt_svc_def serviceDefinition[2];
 
       uint16_t stepCountHandle;
       uint16_t motionValuesHandle;
+      uint16_t sleepValuesHandle;
       std::atomic_bool stepCountNoficationEnabled {false};
       std::atomic_bool motionValuesNoficationEnabled {false};
+      std::atomic_bool sleepValuesNoficationEnabled {false};
     };
   }
 }
