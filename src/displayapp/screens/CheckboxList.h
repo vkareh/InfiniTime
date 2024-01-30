d082ff7b834313be8822b62fe4793bca81be99b4
Victor Kareh <vkareh@redhat.com>
2024-01-30 10:57:47 -0500

    RadioList

--- src/displayapp/screens/CheckboxList.h
+++ src/displayapp/screens/CheckboxList.h
@@ -1,44 +1,51 @@
 #pragma once
 
-#include "displayapp/apps/Apps.h"
 #include "displayapp/screens/Screen.h"
 #include <array>
-#include <cstdint>
 #include <functional>
 #include <lvgl/lvgl.h>
-#include <memory>
 #include "displayapp/widgets/PageIndicator.h"
 
 namespace Pinetime {
   namespace Applications {
     namespace Screens {
       class CheckboxList : public Screen {
-      public:
-        static constexpr size_t MaxItems = 4;
+        public:
+          static constexpr size_t MaxItems = 4;
 
-        struct Item {
-          const char* name;
-          bool enabled;
-        };
+          struct Item {
+            const char* name;
+            bool enabled;
+          };
 
-        CheckboxList(const uint8_t screenID,
-                     const uint8_t numScreens,
-                     const char* optionsTitle,
-                     const char* optionsSymbol,
-                     uint32_t originalValue,
-                     std::function<void(uint32_t)> OnValueChanged,
-                     std::array<Item, MaxItems> options);
-        ~CheckboxList() override;
-        void UpdateSelected(lv_obj_t* object, lv_event_t event);
+          CheckboxList(const uint8_t screenID,
+                       const uint8_t numScreens,
+                       const char* optionsTitle,
+                       const char* optionsSymbol,
+                       uint32_t originalValue,
+                       std::function<void(uint32_t)> OnValueChanged,
+                       std::array<Item, MaxItems> options);
+          ~CheckboxList() override;
 
-      private:
-        const uint8_t screenID;
-        std::function<void(uint32_t)> OnValueChanged;
-        std::array<Item, MaxItems> options;
-        std::array<lv_obj_t*, MaxItems> cbOption;
-        uint32_t value;
+          void RenderScreen(const uint8_t numScreens,
+                            const char* optionsTitle,
+                            const char* optionsSymbol);
 
-        Widgets::PageIndicator pageIndicator;
+          virtual void RenderOptions(const uint8_t screenID,
+                             uint32_t originalValue,
+                             std::array<Item, MaxItems> options);
+
+          void UpdateSelected(lv_obj_t* object, lv_event_t event);
+
+        protected:
+          lv_obj_t* container1;
+          const uint8_t screenID;
+          std::function<void(uint32_t)> OnValueChanged;
+          std::array<Item, MaxItems> options;
+          std::array<lv_obj_t*, MaxItems> cbOption;
+          uint32_t value;
+
+          Widgets::PageIndicator pageIndicator;
       };
     }
   }
