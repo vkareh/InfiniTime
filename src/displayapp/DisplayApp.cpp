1a537cf6d26fb264b741b52349a61d4d1e9d507b
cyberneel <neelesh.chevuri25@gmail.com>
2024-11-24 16:32:13 -0600

    Undid some edits that were for testing

--- src/displayapp/DisplayApp.cpp
+++ src/displayapp/DisplayApp.cpp
@@ -409,6 +409,8 @@ void DisplayApp::Refresh() {
         }
         motorController.RunForDuration(infiniSleepController.gradualWakeVibrationDurations[-1 + infiniSleepController.gradualWakeStep]);
 
+        infiniSleepController.UpdateGradualWake();
+
         NRF_LOG_INFO("Gradual wake triggered");
 
         break;
